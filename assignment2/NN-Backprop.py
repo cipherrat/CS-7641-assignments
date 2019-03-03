6cf5d564f69c663c6df91baba5b5fc62b8df5395
Ratish <ratish@gatech.edu>
2019-03-03 18:25:16 -0500

    Switching changes back to first data set SpamData

--- assignment2/NN-Backprop.py
+++ assignment2/NN-Backprop.py
@@ -13,9 +13,9 @@ from func.nn.activation import RELU
 from base import *
 
 # Network parameters found "optimal" in Assignment 1
-INPUT_LAYER = 15
-HIDDEN_LAYER1 = 244
-HIDDEN_LAYER2 = 244
+INPUT_LAYER = 57
+HIDDEN_LAYER1 = 57
+HIDDEN_LAYER2 = 57
 OUTPUT_LAYER = 1
 TRAINING_ITERATIONS = 3000
 OUTFILE = OUTPUT_DIRECTORY + '/NN_OUTPUT/NN_{}_LOG.csv'
--- assignment2/base.py
+++ assignment2/base.py
@@ -9,9 +9,8 @@ from java.lang import Math
 __all__ = ['DS_NAME', 'TEST_DATA_FILE', 'TRAIN_DATA_FILE', 'VALIDATE_DATA_FILE', 'OUTPUT_DIRECTORY',
            'initialize_instances', 'error_on_data_set', 'train']
 
-#DS_NAME = 'SpamData'
-
-DS_NAME = 'CreditApprovalData'
+DS_NAME = 'SpamData'
+#DS_NAME = 'CreditApprovalData'
 
 TEST_DATA_FILE = '../data/{}_test.csv'.format(DS_NAME)
 TRAIN_DATA_FILE = '../data/{}_train.csv'.format(DS_NAME)
